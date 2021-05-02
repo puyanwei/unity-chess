0b524c4339fb80e3f31f00d66e54ef1a3c32da17
obarker <o.j.barker@hotmail.co.uk>
2021-05-02 19:26:52 +0100

    onMouse color change added.

---
+++ Chess/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Movement : MonoBehaviour
+
+{
+
+    public Color hoverColor;
+    private Renderer rend;
+    private Color startColor;
+    
+
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+        Debug.Log("you hoverd over the pawn!");
+
+     }
+
+
+    private void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        rend.material.color = hoverColor;
+        Debug.Log("you hoverd over the pawn!");
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+        Debug.Log("you left over the pawn!");
+    }
+
+
+
+
+}
