ef690192003e49a142ece45b8f562de28b4b78c2
obarker <o.j.barker@hotmail.co.uk>
2021-05-08 17:24:44 +0100

    test rays started - not working properly at the moment.

--- Chess/Assets/Scripts/3dChess/PawnMovement.cs
+++ Chess/Assets/Scripts/3dChess/PawnMovement.cs
@@ -5,49 +5,97 @@ using UnityEngine;
 public class PawnMovement : MonoBehaviour
 {
     Ray ray;
+    Ray pieceRay;
     RaycastHit hit;
-
+    // test
     public GameObject piece;
     public GameObject square;
     private Vector3 movement;
     public GameObject kill;
-    public GameObject shadow;
-    public GameObject[] shadows;
-    private Vector3 sMovement;
+    private Vector3 allowedMovement;
+    public Color hoverColor;
+    private Renderer rend;
+    private Color startColor;
+    private bool isHover = false;
+    private string cubeInFront;
 
     void Start()
     {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
         movement = new Vector3(0, 0, 2);
         piece.transform.position = movement;
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, piece.transform.position + allowedMovement);
+    }
+
     void Update()
     {
-        shadows = GameObject.FindGameObjectsWithTag("shadow");
-
 
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        pieceRay = Camera.main.ScreenPointToRay(piece.transform.position + allowedMovement);
 
-            if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform.name != piece.name)
-                {
-
-                    piece.transform.position = movement;
-                    movement += new Vector3(0, 0, 1);
-
-                }
-            }
-        }   
- 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.name == kill.name)
+        if (isHover)
         {
-            GameObject.Destroy(piece);
+           
+            Physics.Raycast(pieceRay, out hit);
+            cubeInFront = hit.transform.name;
+            Debug.Log(cubeInFront);
+
+
+
+        }
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name != piece.name)
+            {
+
+                piece.transform.position = movement;
+                movement += new Vector3(0, 0, 1);
+                
+
+            }
             
         }
-        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == kill.name)
+        {
+            GameObject.Destroy(piece);
+
+        }
+
+    }
+
+    void OnMouseOver()
+    {
+        rend.material.color = hoverColor;
+        isHover = true;
+
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+        isHover = false;
+
+    }
+
+
+    void functiontohit(Vector3 position)
+    {
+        ray = Camera.main.ScreenPointToRay(piece.transform.position + position);
+        Physics.Raycast(ray, out hit);
+
     }
 
 }
+
+
