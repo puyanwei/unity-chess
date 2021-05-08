27eba0e2943753a314f6e064dbcda479de579e16
Puyan Wei <puyanwei@gmail.com>
2021-05-08 15:54:08 +0100

    Added hover logic and raycasting one block ahead for pawn

--- Chess/Assets/Scripts/3dChess/PawnMovement.cs
+++ Chess/Assets/Scripts/3dChess/PawnMovement.cs
@@ -9,45 +9,71 @@ public class PawnMovement : MonoBehaviour
 
     public GameObject piece;
     public GameObject square;
-    private Vector3 movement;
     public GameObject kill;
-    public GameObject shadow;
-    public GameObject[] shadows;
-    private Vector3 sMovement;
+    public Color hoverColor;
+    private Color startColor;
+    private Renderer rend;
+    private Vector3 movement;
+
+    private Vector3 allowedMovement = new Vector3(0, 0, 1);
+
+    private bool isHovered = false;
 
     void Start()
     {
         movement = new Vector3(0, 0, 2);
         piece.transform.position = movement;
+
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
     }
 
     void Update()
     {
-        shadows = GameObject.FindGameObjectsWithTag("shadow");
-
-
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit))
+        if (isHovered)
+        {
+            ShowAllowedMovements(allowedMovement);
+        }
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name != piece.name)
             {
-                if (hit.transform.name != piece.name)
-                {
-
-                    piece.transform.position = movement;
-                    movement += new Vector3(0, 0, 1);
-
-                }
+                piece.transform.position = movement;
+                movement += allowedMovement;
             }
-        }   
+        }        
+    }   
  
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == kill.name)
         {
             GameObject.Destroy(piece);
-            
         }
         
+    }
+
+
+    void OnMouseOver()
+    {
+        rend.material.color = hoverColor;
+        isHovered = true;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+        isHovered = false;
+    }
+
+    void ShowAllowedMovements(Vector3 position)
+    {
+        ray = Camera.main.ScreenPointToRay(piece.transform.position + position);
+        Physics.Raycast(ray, out hit);
+        Debug.Log("There's a chess block in front");
     }
 
 }
